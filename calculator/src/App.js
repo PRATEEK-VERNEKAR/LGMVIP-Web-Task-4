a8f74c81b9e4939e3bcfe57ef6662f13ffd2ecf1
unknown <mprateekvernekar189@gmail.com>
2023-09-28 20:56:43 +0530

    Completed

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,71 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 import {evaluate} from 'mathjs';
 
 function App() {
   const [display,setDisplay]=useState("");
-  const [operatorOnNot,setOperatorOrNot]=useState(false);
-  const [decimalAllow,setDecimalAllow]=useState(false);
-  const [mulDevAllow,setMulDevAllow]=useState(true);
+  const [invalid,setInvalid]=useState(false);
 
   const handleNumber=(num)=>{
-    const tempString=display+num;
-    setDisplay(tempString)
-
-    if(num==="+" || num==="-" || num==="*" || num==='/' || num==='='){
-      setOperatorOrNot(true);
-      setDecimalAllow(false);
+    if(invalid){
+      console.log(invalid);
+      setDisplay(num);
+      setInvalid(false);
     }
     else{
-      setOperatorOrNot(false);
+      const tempString=display+num;
+      setDisplay(tempString)
     }
-    
-    if(num==='.'){
-      setOperatorOrNot(true);
-      setDecimalAllow(true);
-    }
-
-    setMulDevAllow(false);
   }
 
   const handleEquate=()=>{
-    let ans=evaluate(display);
-    // ans=math.format(ans,{precision:14});
-    ans=String(ans)
-    setDisplay(ans);
-    setDecimalAllow(false);
-    
-    if(display==='0'){
-      setDecimalAllow(true);
+    try{
+
+      let ans=evaluate(display);
+      // ans=math.format(ans,{precision:14});
+      ans=String(ans)
+      setDisplay(ans);
+    }
+    catch(e){
+      setInvalid(true);
+      setDisplay("Invalid Input");
     }
   }
 
+  const handleBackSpace=()=>{
+    let tempString=display;
+    tempString=tempString.slice(0,-1);
+    setDisplay(tempString);
+  }
+
   return (
-    <div class="App">
-      <div class="Region">
-        <div class='output-panel'>
-          <input id='output-field' readOnly value={display} placeholder='0'></input>
-          {/* <button>C</button> */}
-          <button onClick={()=>{setDisplay("")}}>C</button>
-        </div>
-        <div class='inputs'>
-          <button onClick={()=>{handleNumber("1")}} class="numbers">1</button>
-          <button onClick={()=>{handleNumber("2")}} class="numbers">2</button>
-          <button onClick={()=>{handleNumber("3")}} class="numbers">3</button>
-          <button onClick={()=>{handleNumber("+")}} class="operators" disabled={operatorOnNot}>+</button>
-          <button onClick={()=>{handleNumber("4")}} class="numbers">4</button>
-          <button onClick={()=>{handleNumber("5")}} class="numbers">5</button>
-          <button onClick={()=>{handleNumber("6")}} class="numbers">6</button>
-          <button onClick={()=>{handleNumber("-")}} class="operators" disabled={operatorOnNot}>-</button>
-          <button onClick={()=>{handleNumber("7")}} class="numbers">7</button>
-          <button onClick={()=>{handleNumber("8")}} class="numbers">8</button>
-          <button onClick={()=>{handleNumber("9")}} class="numbers">9</button>
-          <button onClick={()=>{handleNumber("*")}} class="operators" disabled={operatorOnNot || mulDevAllow}>*</button>
-          <button onClick={()=>{handleNumber(".")}} class="decimal" disabled={decimalAllow}>.</button>
-          <button onClick={()=>{handleNumber("0")}} class="numbers">0</button>
-          <button onClick={()=>{handleNumber("/")}} class="operators" disabled={operatorOnNot || mulDevAllow}>/</button>
-          <button onClick={()=>{handleEquate()}} class="equal" disabled={operatorOnNot}>=</button>
+    <div className="App">
+      <div className="Region">
+        <input id='output-field' readOnly value={display} placeholder='0'></input>
+        <div className='inputs'>
+          <button onClick={()=>{handleBackSpace()}} className="equal ">&lt;</button>
+          <div></div>
+          <div></div>
+          <button onClick={()=>{setDisplay("")}} className='close'>C</button>
+          <button onClick={()=>{handleNumber("1")}} className="numbers">1</button>
+          <button onClick={()=>{handleNumber("2")}} className="numbers">2</button>
+          <button onClick={()=>{handleNumber("3")}} className="numbers">3</button>
+          <button onClick={()=>{handleNumber("+")}} className="operators ">+</button>
+          <button onClick={()=>{handleNumber("4")}} className="numbers">4</button>
+          <button onClick={()=>{handleNumber("5")}} className="numbers">5</button>
+          <button onClick={()=>{handleNumber("6")}} className="numbers">6</button>
+          <button onClick={()=>{handleNumber("-")}} className="operators ">-</button>
+          <button onClick={()=>{handleNumber("7")}} className="numbers">7</button>
+          <button onClick={()=>{handleNumber("8")}} className="numbers">8</button>
+          <button onClick={()=>{handleNumber("9")}} className="numbers">9</button>
+          <button onClick={()=>{handleNumber("*")}} className="operators ">*</button>
+          <button onClick={()=>{handleNumber(".")}} className="decimal">.</button>
+          <button onClick={()=>{handleNumber("0")}} className="numbers">0</button>
+          <button onClick={()=>{handleEquate()}} className="equal">=</button>
+          <button onClick={()=>{handleNumber('/')}} className="operators ">/</button>
         </div>
       </div>
     </div>
